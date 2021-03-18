f5d165b1dcbdb80084db255ce172fbe82a78ccbe
1165518359@qq.com <1165518359@qq.com>
2021-03-18 18:08:01 +0800

    axios

---
+++ mock/demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="test"></div>
+</template>
+<script>
+  import { getCurrentInstance } from 'vue'
+
+  export default {
+    name: 'Home',
+    setup() {
+      const { ctx } = getCurrentInstance()
+      console.log(ctx)
+
+      ctx.$Axios.get('/mock/comment/get.action', {}).then((res) => {
+        console.log(res.data)
+      })
+    }
+  }
+</script>
